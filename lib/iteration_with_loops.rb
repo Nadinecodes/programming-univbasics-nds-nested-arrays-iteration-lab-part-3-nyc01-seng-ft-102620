746fffd7d2b189f9e426c199453df7571dccc8dc
Nadine Edwards <edwardsnadine1985@gmail.com>
2020-10-05 01:11:28 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,7 +10,7 @@ def join_nested_strings(mixed_data)
     element_index = 0
     while element_index < mixed_data[row_index].count do
       if mixed_data[row_index][element_index].class=="".class
-      string = mixed_data[row_index][element_index]
+      string = string+ " " + mixed_data[row_index][element_index]
     end
       element_index += 1
     end
